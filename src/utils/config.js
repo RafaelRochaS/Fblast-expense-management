806d328f7294f495533e00fe64d9b35a82292b81
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-04-11 17:21:44 -0300

    Add basic logging

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,3 +7,4 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config({path: path.resolve(__dirname, '..', '.env')});
 
 export const PORT = process.env.PORT;
+export const LOG_FORMAT = process.env.REQUEST_LOG_FORMAT || 'common';
