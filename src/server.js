f0615f4f228db11c1b7d4e9711d8291ffc7e0cb7
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-04-11 17:31:12 -0300

    Fix linting

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes.js';
-import { default as logger} from 'morgan';
-import { LOG_FORMAT } from './utils/config.js'
+import {default as logger} from 'morgan';
+import {LOG_FORMAT} from './utils/config.js';
 import * as util from 'util';
 
 const app = express();
@@ -12,12 +12,12 @@ app.use(cors());
 app.use(logger(LOG_FORMAT));
 app.use(routes);
 
-process.on('uncaughtException', function (err) {
-    console.error(`I've crashed!!! - ${(err.stack || err)}`);
+process.on('uncaughtException', function(err) {
+  console.error(`I've crashed!!! - ${(err.stack || err)}`);
 });
 
 process.on('unhandledRejection', (reason, p) => {
-    console.error(`Unhandled Rejection at: ${util.inspect(p)} reason: ${reason}`);
+  console.error(`Unhandled Rejection at: ${util.inspect(p)} reason: ${reason}`);
 });
 
 export default app;
